84297decc5d9b8c97b878c7399d7e4105edc5751
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-01 19:54:38 +0530

    center

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import React from 'react'
 //called from main.jsx
 const App = () => {
   return (
-    <div className='w-full h-screen bg-zinc-800'>
+    <div className='relative w-full h-screen bg-zinc-800'>
       {/* h-screen: height will be size of screen */}
       {/* leading-none: will reduce the line height, tacking-tight will reduce the letter spacing : 
       you can see them on docs of tailwindss just search them there */}
-      <h1 className='text-[13vw] leading-none tracking-tight'>My Docs</h1>
+      {/* absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]: 
+      will make the text in center and it's absolute to outer div it's relative */}
+      <h1 className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tight'>My Docs</h1>
     </div>
   )
 }
