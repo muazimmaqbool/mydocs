f2f0f1e68c4d640bb87f36035f05470b95de7d1f
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-01 19:52:26 +0530

    heading added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ const App = () => {
   return (
     <div className='w-full h-screen bg-zinc-800'>
       {/* h-screen: height will be size of screen */}
+      {/* leading-none: will reduce the line height, tacking-tight will reduce the letter spacing : 
+      you can see them on docs of tailwindss just search them there */}
+      <h1 className='text-[13vw] leading-none tracking-tight'>My Docs</h1>
     </div>
   )
 }
