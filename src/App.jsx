080ddee088159c42f57584c5102dd52a5645ede2
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-01 19:42:48 +0530

    bg change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 //called from main.jsx
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-full h-screen bg-zinc-800'>
+      {/* h-screen: height will be size of screen */}
+    </div>
   )
 }
 
