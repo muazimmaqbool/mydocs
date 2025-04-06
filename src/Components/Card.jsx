304c8b4fc0728147fa3da997cd0581b60a0cf316
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-06 22:25:00 +0530

    text added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,9 +4,10 @@ import { FaRegFileAlt } from "react-icons/fa";
 //called from Foreground.jsx
 const Card = () => {
   return (
-    <div className='w-60 h-72 bg-zinc-900/10 p-5 text-white rounded-[20px]'>
-        {/* bg-zinc-900/10 in this 10 means 10% opacity */}
+    <div className='relative w-60 h-72 bg-zinc-900/90 p-5 text-white rounded-[20px]'>
+        {/* bg-zinc-900/10 in this 90 means 90% opacity */}
         <FaRegFileAlt/>
+        <p className='text-sm m-5 font-semibold leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
     </div>
 
   )
