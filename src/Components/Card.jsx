3422c8a4b1e8ea54c5d9dc86631d3b3c364ab595
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-06 22:22:14 +0530

    icon added in card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
 
 //called from Foreground.jsx
 const Card = () => {
   return (
-    <div className='w-60 h-72 bg-zinc-900/10 rounded-[20px]'>
+    <div className='w-60 h-72 bg-zinc-900/10 p-5 text-white rounded-[20px]'>
         {/* bg-zinc-900/10 in this 10 means 10% opacity */}
-        
+        <FaRegFileAlt/>
     </div>
 
   )
