f906f9eb36e8527a91ce5b3532cdfb78c9fc51e4
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-08 22:20:55 +0530

    tab div added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -11,13 +11,17 @@ const Card = () => {
       <p className="text-sm mt-5 font-semibold leading-tight">
         Lorem ipsum dolor sit amet consectetur adipisicing.
       </p>
-      <div className="footer absolute bottom-0 px-8 w-full left-0 py-3">
-        <div className="flex items-center justify-between mb-5">
+      <div className="footer absolute bottom-0  w-full left-0 ">
+        <div className="flex items-center justify-between mb-3 py-3 px-8 ">
           <h5>.4mb</h5>
           <span className="flex items-center justify-center w-7 h-7 bg-zinc-600 rounded-full">
             <LuDownload size="0.7em" color="#fff"/>
           </span>
         </div>
+      <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
+        <h3 className="text-sm font-semibold">Download Now</h3>
+      </div>
+
       </div>
     </div>
   );
