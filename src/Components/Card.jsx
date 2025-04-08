9ca92d356bebb5ca7c806d2a9a08beaee0a3b89c
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-08 22:21:32 +0530

    card radius fixed

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,7 +5,7 @@ import { LuDownload } from "react-icons/lu";
 //called from Foreground.jsx
 const Card = () => {
   return (
-    <div className="relative w-60 h-72 bg-zinc-900/90 px-8 py-10 text-white rounded-[50px] overflow-hidden">
+    <div className="relative w-60 h-72 bg-zinc-900/90 px-8 py-10 text-white rounded-[45px] overflow-hidden">
       {/* bg-zinc-900/10 in this 90 means 90% opacity */}
       <FaRegFileAlt />
       <p className="text-sm mt-5 font-semibold leading-tight">
