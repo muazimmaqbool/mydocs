5e6f22339c5c3a5ac4db681a160351c9f2a6866f
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-09 22:23:29 +0530

    data for card compo

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,17 +3,18 @@ import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
 
 //called from Foreground.jsx
-const Card = () => {
+const Card = ({data}) => {
+
   return (
     <div className="relative w-60 h-72 bg-zinc-900/90 px-8 py-10 text-white rounded-[45px] overflow-hidden">
       {/* bg-zinc-900/10 in this 90 means 90% opacity */}
       <FaRegFileAlt />
       <p className="text-sm mt-5 font-semibold leading-tight">
-        Lorem ipsum dolor sit amet consectetur adipisicing.
+        {data?.desc}
       </p>
       <div className="footer absolute bottom-0  w-full left-0 ">
         <div className="flex items-center justify-between mb-3 py-3 px-8 ">
-          <h5>.4mb</h5>
+          <h5>{data?.fileSize}</h5>
           <span className="flex items-center justify-center w-7 h-7 bg-zinc-600 rounded-full">
             <LuDownload size="0.7em" color="#fff"/>
           </span>
--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
 //called from App.jsx
 //the cards will come inside this component
 const Foreground = () => {
+  const data = [
+    {
+      desc: "This is the test description inside the card component",
+      fileSize: ".4mb",
+      close: true,
+      tab: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+  ];
   return (
     <div className="fixed top-0 left-0 z-[3] w-full h-full">
-        {/* bg-sky-800/50 means 50% transparent, 50 means 50% opacity */}
-    
-        <Card/>
-        
+      {/* bg-sky-800/50 means 50% transparent, 50 means 50% opacity */}
+      {
+        data.map((item,index)=>(
+          <Card data={item}/>
+        ))
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Foreground
+export default Foreground;
