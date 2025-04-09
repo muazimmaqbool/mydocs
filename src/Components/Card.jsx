6c6e5f7488c92adea0bea639b2f726324b63c178
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-09 22:30:27 +0530

    style fix

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,7 +6,7 @@ import { IoMdClose } from "react-icons/io";
 //called from Foreground.jsx
 const Card = ({ data }) => {
   return (
-    <div className="relative w-60 h-72 bg-zinc-900/90 px-8 py-10 text-white rounded-[45px] overflow-hidden">
+    <div className="relative w-60 h-72 flex-shrink-0 bg-zinc-900/90 px-8 py-10 text-white rounded-[45px] overflow-hidden">
       {/* bg-zinc-900/10 in this 90 means 90% opacity */}
       <FaRegFileAlt />
       <p className="text-sm mt-5 font-semibold leading-tight">{data?.desc}</p>
--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -15,9 +15,29 @@ const Foreground = () => {
         tagColor: "green",
       },
     },
+    {
+      desc: "This is the test description inside the card component",
+      fileSize: "5mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "This is the test description inside the card component",
+      fileSize: "3mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
   ];
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
+    <div className="fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap p-10">
       {/* bg-sky-800/50 means 50% transparent, 50 means 50% opacity */}
       {
         data.map((item,index)=>(
