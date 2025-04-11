ab19d1b21410f8fd83ea5f247a8a6b78882ee21c
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-11 22:53:58 +0530

    subtitles

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -16,6 +16,15 @@ const Background = () => {
       <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[12vw] leading-none tracking-tight font-semibold text-zinc-900">
         My Tasks.
       </h1>
+      <h4>
+      Organize. Prioritize. Done.
+      </h4>
+      <h4>
+      Your day, your way.
+      </h4>
+      <h4>
+      Plan it. Do it
+      </h4>
     </div>
   );
 };
