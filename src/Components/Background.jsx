7b37d27b7afb81ce8c0ae435f01ddb81a5d02c20
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-12 15:38:35 +0530

    name changed

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -16,7 +16,7 @@ const Background = () => {
       {/* text-[12vw]: will make the text responsive */}
       
       <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[70%] text-[12vw] leading-none tracking-tight font-semibold text-zinc-900">
-        My Tasks.
+        My Docs.
       </h1>
       <div className="flex items-center justify-center gap-5 h-screen mt-30 box-border text-zinc-500 text-md">
       <h4>
