a7bd0dad7a35f9476ebdc3f2472157eeb2c65be7
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-16 22:29:32 +0530

    Modal removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Background from "./Components/Background";
 import Foreground from "./Components/Foreground";
-import Modal from "./Components/Modal";
 
 //docs of tailwind css: https://tailwindcss.com/docs/installation/using-vite
 
@@ -10,7 +9,6 @@ const App = () => {
   return (
     <div className="relative w-full h-screen bg-zinc-800">
       {/* h-screen: height will be size of screen */}
-      <Modal/>
       <Background/>
       <Foreground/>
     </div>
